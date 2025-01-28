7794f99648db256d01ad5bc8a8e431522c05043f
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2025-01-28 17:01:14 +0100

    tmp: Update `ft_lstadd_back_eff()` doc

--- libraries/libft/src/lists/singly_linked/ft_lstadd_back_eff.c
+++ libraries/libft/src/lists/singly_linked/ft_lstadd_back_eff.c
@@ -6,7 +6,7 @@
 /*   By: ldulling <ldulling@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/11 00:35:15 by ldulling          #+#    #+#             */
-/*   Updated: 2024/06/07 19:10:00 by ldulling         ###   ########.fr       */
+/*   Updated: 2025/01/28 16:59:53 by ldulling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,6 @@
  * @param lst     The address of the pointer to the first node of the list.
  * @param tail    The address of the pointer to the last node of the list.
  * @param new     The new node to be added to the list.
- *
- * @return        This function does not return a value.
  *
  */
 void	ft_lstadd_back_eff(t_list **lst, t_list **tail, t_list *new)
